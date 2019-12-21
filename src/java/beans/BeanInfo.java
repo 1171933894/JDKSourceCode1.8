c76b6fe856573c93592bf630559c637332e2d46c
1171933894@qq.com <1171933894@qq.com>
2019-12-21 22:24:57 +0800

    commit

--- src/java/beans/BeanInfo.java
+++ src/java/beans/BeanInfo.java
@@ -2,25 +2,6 @@
  * Copyright (c) 1996, 2012, Oracle and/or its affiliates. All rights reserved.
  * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
  */
 
 package java.beans;
